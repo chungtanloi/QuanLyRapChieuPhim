03a8e6a31b8cf3c224abe19e55b5945644c9a7cc
Phạm Huyền Trân <155203343+Bhjtran@users.noreply.github.com>
2025-11-16 05:36:10 +0700

    Add files via upload

---
+++ CSDL/Trigger Trung Khuyen Mai.sql
@@ -0,0 +1,16 @@
+DELIMITER //
+CREATE TRIGGER tr_BeforeInsert_KhuyenMai_CheckDuplicate
+BEFORE INSERT ON khuyen_mai
+FOR EACH ROW
+BEGIN
+    DECLARE existing_count INT;
+    
+    SELECT COUNT(*) INTO existing_count 
+    FROM khuyen_mai 
+    WHERE ma_code = NEW.ma_code;
+    
+    IF existing_count > 0 THEN
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'Mã khuyến mãi đã tồn tại';
+    END IF;
+END //
